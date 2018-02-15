2cfc0f48118d607185a7195597903f5b6065a1f7
Abilash Menon <abilashrmenon@icloud.com>
2018-02-15 22:12:17 +0800

    Constants
    Constant Keys

---
+++ app/src/main/java/com/abilashmenon/computerarchitecture/Keys.java
@@ -0,0 +1,17 @@
+package com.abilashmenon.computerarchitecture;
+
+/**
+ * Created by abilashmenon on 13/2/18.
+ */
+
+public class Keys {
+    public static final String FCFS = "FCFS";
+    public static final String SJFNP = "SJFNP";
+    public static final String PriorityNP = "PriorityNP";
+    public static final String SJFP = "SJFP";
+    public static final String PriorityP = "PriorityP";
+    public static final String RR = "RR";
+    public static final String FirstFitMemory = "FF";
+    public static final String WorstFitMemory = "WF";
+    public static final String BestFitMemory = "BF";
+}
